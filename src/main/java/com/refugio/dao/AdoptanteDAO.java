b1ac18570b6b89fd817c59feb3cf4e5c23702ef7
Andrei <andrei.iushkov@davinci.edu.ar>
2025-07-12 23:12:28 -0300

    ref: remove redundant DAO implementations

--- src/main/java/com/refugio/dao/AdoptanteDAO.java
+++ src/main/java/com/refugio/dao/AdoptanteDAO.java
@@ -2,9 +2,11 @@ package com.refugio.dao;
 
 import com.refugio.persona.Adoptante;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
-public interface AdoptanteDAO {
-    void guardar(Adoptante adoptante);
-    Adoptante buscarPorNombre(String nombre);
-    List<Adoptante> obtenerTodos();
+@Repository
+public interface AdoptanteDAO extends JpaRepository<Adoptante, Long> {
+    Optional<Adoptante> findByNombre(String nombre);
 }
--- src/main/java/com/refugio/dao/EmpleadoDAO.java
+++ src/main/java/com/refugio/dao/EmpleadoDAO.java
@@ -1,10 +1,10 @@
 package com.refugio.dao;
 
 import com.refugio.persona.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface EmpleadoDAO {
-    void guardar(Empleado empleado);
-    Empleado buscarPorNombre(String nombre);
-    List<Empleado> obtenerTodos();
+@Repository
+public interface EmpleadoDAO extends JpaRepository<Empleado, Long> {
 }
